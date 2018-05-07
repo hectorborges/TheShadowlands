1c3f49efccc31d834e00f61b2a9bebed460684d1
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-05-07 18:28:42 -0500

    Added Utility script

---
+++ Assets/Game/Scripts/Utility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility : MonoBehaviour
+{
+    public static float CheckDistance(Vector3 point1, Vector3 point2)
+    {
+        Vector3 heading;
+        float distance;
+        Vector3 direction;
+        float distanceSquared;
+
+        heading.x = point1.x - point2.x;
+        heading.y = point1.y - point2.y;
+        heading.z = point1.z - point2.z;
+
+        distanceSquared = heading.x * heading.x + heading.y * heading.y + heading.z * heading.z;
+        distance = Mathf.Sqrt(distanceSquared);
+
+        direction.x = heading.x / distance;
+        direction.y = heading.y / distance;
+        direction.z = heading.z / distance;
+        return distance;
+    }
+}
