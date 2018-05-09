f6c8adde9e356b31e4617e3acaa38fa317a8552e
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-05-09 06:50:57 -0500

    Added hit effect for enemies

---
+++ Assets/EnemyAnimationBridgeEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationBridgeEvent : MonoBehaviour
+{
+    public EffectAnimationEvent[] abilityEffectAnimationEvents;
+
+    public void AbilityEffect(string _effectName)
+    {
+        foreach (EffectAnimationEvent abilityEffect in abilityEffectAnimationEvents)
+        {
+            if (abilityEffect.effectName == _effectName)
+            {
+                abilityEffect.effect.SetActive(true);
+                StartCoroutine(DisableEffectAfter(abilityEffect.effect, abilityEffect.disableAfter));
+                break;
+            }
+        }
+    }
+
+    IEnumerator DisableEffectAfter(GameObject effectToDisable, float disableAfter)
+    {
+        yield return new WaitForSeconds(disableAfter);
+        effectToDisable.SetActive(false);
+    }
+}
+
+[System.Serializable]
+public class EffectAnimationEvent
+{
+    public string effectName;
+    public GameObject effect;
+    public float disableAfter;
+}
