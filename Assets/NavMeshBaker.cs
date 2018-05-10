a095197edebc6d55c6b62e26d5b9c02c640abc2b
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-05-09 19:39:16 -0500

    working on runtime navmesh

---
+++ Assets/NavMeshBaker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+	void Start ()
+    {
+        GetComponent<NavMeshSurface>().BuildNavMesh();
+	}
+}
