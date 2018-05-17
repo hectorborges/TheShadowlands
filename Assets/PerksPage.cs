178d6572cb6fb5a52441e4e16be3bd35aa60c679
hectorborges <hectorborges94@gmail.com>
2018-05-16 23:04:52 -0500

    Added a weapons vault ui

---
+++ Assets/PerksPage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerksPage : MonoBehaviour
+{
+    public enum WeaponType
+    {
+        Axe,
+        Swords,
+        Shield,
+        Rifle,
+        Magic
+    };
+
+    public WeaponType weaponType;
+    public Image perkSlots;
+}
---
+++ Assets/WeaponsVault.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponsVault : MonoBehaviour
+{
+    public PerksPage[] perksPages;
+    public Image[] pageSelections;
+
+    public Color unselectedColor;
+    public Color selectedColor;
+
+    PerksPage currentPerksPageSelected;
+    Image currentPageSelected;
+
+    private void Start()
+    {
+        currentPerksPageSelected = perksPages[0];
+        currentPageSelected = pageSelections[0];
+
+        currentPerksPageSelected.gameObject.SetActive(true);
+        currentPageSelected.color = selectedColor;
+    }
+
+    public void SelectPage(string pageSelected)
+    {
+        for(int i = 0; i < perksPages.Length; i++)
+        {
+            if(pageSelected == perksPages[i].weaponType.ToString())
+            {
+                currentPerksPageSelected.gameObject.SetActive(false);
+                currentPageSelected.color = unselectedColor;
+
+                currentPerksPageSelected = perksPages[i];
+                currentPageSelected = pageSelections[i];
+
+                currentPerksPageSelected.gameObject.SetActive(true);
+                currentPageSelected.color = selectedColor;
+            }
+        }
+    }
+}
